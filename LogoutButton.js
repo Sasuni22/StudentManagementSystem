c0e03a62c4f76c545c9d90ebe6fb9b78a64ad8f6
Sasuni Dilrukshi <pathiragesasuni2124@gmail.com>
2026-03-13 10:27:44 +0530

    Update LogoutButton.js

--- LogoutButton.js
+++ LogoutButton.js
@@ -1,14 +1,13 @@
-// /components/LogoutButton.js
+
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/logoutButton.css";
- // Ensure this CSS is applied
-
+ 
 export default function LogoutButton() {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Perform logout logic (e.g., clear session, etc.)
+    
     navigate("/"); // Redirect to home or login page after logout
   };
 
@@ -20,3 +19,4 @@ export default function LogoutButton() {
     </div>
   );
 }
+
